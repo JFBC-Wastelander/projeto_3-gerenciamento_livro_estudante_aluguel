0976e70831605726fa5987ab44555cc1a9b199af
Jean <JFBimCopinski@gmail.com>
2024-09-05 08:04:03 -0300

    bugfix: working code

--- src/models/aluguel_model.js
+++ src/models/aluguel_model.js
@@ -3,23 +3,23 @@ const livro_control = require("../controllers/livro_controller.js");
 
 let proxId = 1;
 const model = (body, id = proxId++) => {
-  // if (
-  //   body.aluguel != undefined &&
-  //   body.aluguel != "" &&
-  //   body.devolucao != undefined &&
-  //   body.devolucao != "" &&
-  //   estudante_control.show(body.estudanteId) &&
-  //   livro_control.show(body.livroId)
-  // ) {
+  if (
+    body.dataAluguel != undefined &&
+    body.dataAluguel != "" &&
+    body.dataDevolucao != undefined &&
+    body.dataDevolucao != "" &&
+    estudante_control.show(body.estudanteId) &&
+    livro_control.show(body.livroId)
+  ) {
     return {
       id,
-      dataAluguel: body.aluguel,
-      dataDevolucao: body.devolucao,
-      estudanteId: body.estudanteId,
-      livroId: body.livroId
+      dataAluguel: body.dataAluguel,
+      dataDevolucao: body.dataDevolucao,
+      estudanteId: +body.estudanteId,
+      livroId: +body.livroId
     };
   }
-// };
+};
 
 module.exports = model;
 
